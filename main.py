c102c3942fbb3d7bec307095bb2b0d2b9f7e6332
Joshua Hecker <jhecker2001@gmail.com>
2023-11-10 09:54:15 -0600

    Added functions to convert UTC to CST and to determine if the ISS is close to the user's current position.

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import requests
 from datetime import datetime
 
-MY_LAT = 32.543390 # Your latitude
-MY_LONG = -94.784170 # Your longitude
+
+def utc_to_cst(hr):
+    local_hr = hr - 6
+    return local_hr
+
+
+MY_LAT = 32.543390  # Your latitude
+MY_LONG = -94.784170  # Your longitude
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
@@ -11,7 +17,17 @@ data = response.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
+
 # Your position is within +5 or -5 degrees of the ISS position.
+def is_close():
+    latitude_difference = iss_latitude - MY_LAT
+    longitude_difference = iss_longitude - MY_LONG
+    if 5 >= latitude_difference >= -5:
+        return True
+    elif 5 >= longitude_difference >= -5:
+        return True
+    else:
+        return False
 
 
 parameters = {
@@ -25,6 +41,9 @@ response.raise_for_status()
 data = response.json()
 sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
 sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+
+sunrise = utc_to_cst(sunrise)
+sunset = utc_to_cst(sunset)
 
 time_now = datetime.now()
 
